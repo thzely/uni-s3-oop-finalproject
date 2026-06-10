386e2de7021820c0dac639aec7f832248f10f940
Marianne Fisch <tthzely@gmail.com>
2026-06-10 18:36:32 -0300

    Terminando classe Funcionario e adicionando classe Cliente

--- ProjetoMercado/Model/Funcionario.cs
+++ ProjetoMercado/Model/Funcionario.cs
@@ -1,6 +1,5 @@
 using System;
 namespace Supermarket.model;
-
 public class Funcionario : Pessoa
 {
     public string  Cargo   { get; private set; }
@@ -13,9 +12,19 @@ public class Funcionario : Pessoa
         Cargo   = cargo;
         Salario = salario;
     }
+    public override string Identificar() =>
+        $"Funcionário: {Nome} | Cargo: {Cargo} | Salário: R$ {Salario:F2}";
 
     public override string Identificar() =>
         $"Funcionário: {Nome} | Cargo: {Cargo} | Salário: R$ {Salario:F2}";
 
+    public void ReajustarSalario(decimal percentual)
+    {
+        if (percentual <= 0 || percentual > 30)
+            throw new InvalidOperationException("Reajuste deve estar entre 0% e 30%.");
 
+        decimal aumento = Salario * (percentual / 100m);
+        Salario += aumento;
+        Console.WriteLine($"  {Nome}: reajuste de {percentual}%. Novo salário: R$ {Salario:F2}");
+    }
 }
